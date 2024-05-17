77eddc1116d58001617c9c263fbcaee6c9f840f5
andrefrancisco <asfrancisco@ua.pt>
2024-05-17 14:14:34 +0100

    local

--- src/components/Pages/Erro.js
+++ src/components/Pages/Erro.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Container, Alert, Button, Col, Row } from "reactstrap";
 import '../../gerais.css'
+import { Link } from "react-router-dom";
+
+
 export default function Erro() {
     return (
 
@@ -9,7 +12,9 @@ export default function Erro() {
                 <Col md={7} sm={10}>
                     <h1 className='homeTitle2 text-start'>Error</h1>
                     <h1 className='fs-2 fw-light font-monospace pt-3 text-white'>This page doesn't exist!!</h1>
-                    <Button className='mt-3' href='/'> Back to home </Button>
+                    <Button className='mt-3 '><Link className='text-white text-decoration-none' to='/'> Back to home  </Link> </Button>
+
+                    
                 </Col>
         </Row>
     </Container>
